cc6805eeb2d3bdfc04a63b4c0dd39936a594fabf
liuyug <liuyug@yahoo.cn>
2013-11-21 17:36:16 +0800

    Add rest lexer

--- rsteditor/editor.py
+++ rsteditor/editor.py
@@ -3,9 +3,53 @@ import os.path
 
 from PyQt4 import QtGui, QtCore
 from PyQt4.Qsci import QsciScintilla
-from PyQt4.Qsci import QsciLexerPython, QsciLexerHTML, QsciLexerBash
+from PyQt4.Qsci import QsciLexerCustom, QsciLexerPython, QsciLexerHTML, QsciLexerBash
 
 from rsteditor.util import toUtf8
+
+
+class MyLexerRest(QsciLexerCustom):
+    ID_DEFAULT = 0
+    ID_COMMENT = 1
+
+    def language(self):
+        return 'Rest'
+
+    def description(self, style):
+        if style == self.ID_DEFAULT:
+            return 'ID_DEFAULT'
+        if style == self.ID_COMMENT:
+            return 'ID_COMMENT'
+        print(style)
+        return ''
+
+    def styleText(self, start, end):
+        if not self.editor():
+            return
+        line_s, index_s = self.editor().lineIndexFromPosition(start)
+        line_e, index_e = self.editor().lineIndexFromPosition(end)
+        print(line_s, index_s, line_e, index_e)
+        for line in range(line_s, line_e + 1):
+            offset = self.editor().positionFromLineIndex(line, 0)
+            text = toUtf8(self.editor().text(line))
+            print(text)
+            if text.startswith('.. '):
+                self.startStyling(offset)
+                self.setStyling(len(text), self.ID_COMMENT)
+                print('comment')
+            else:
+                self.startStyling(offset)
+                self.setStyling(len(text), self.ID_DEFAULT)
+                print('default')
+            print(offset, end)
+
+    def color(self, style):
+        if style == self.ID_DEFAULT:
+            return QtGui.QColor('#e00000')
+        if style == self.ID_COMMENT:
+            return QtGui.QColor('#00e000')
+        return super(MyLexerRest, self).defaultColor(style)
+
 
 class FindDialog(QtGui.QDialog):
     findNext = QtCore.pyqtSignal(str, int)
@@ -100,9 +144,9 @@ class Editor(QsciScintilla):
         commit_text = toUtf8(event.commitString())
         if commit_text:
             self.input_count += len(commit_text)
-            if self.input_count > 5:
-                self.lineInputed.emit()
-                self.input_count = 0
+        if self.input_count > 5:
+            self.lineInputed.emit()
+            self.input_count = 0
         return
 
     def keyPressEvent(self, event):
@@ -111,10 +155,10 @@ class Editor(QsciScintilla):
         if (input_text or
             ( event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return )):
             self.input_count += 1
-            if (self.input_count > 5 or
-                    ( event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return )):
-                self.lineInputed.emit()
-                self.input_count = 0
+        if (self.input_count > 5 or
+            ( event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return )):
+            self.lineInputed.emit()
+            self.input_count = 0
         return
 
     def setCopyAvailable(self, yes):
@@ -161,37 +205,37 @@ class Editor(QsciScintilla):
         self.findDialog.exec_()
         self.find_text = self.findDialog.getFindText()
         self.findFirst(self.find_text,
-                False,  # re
-                self.findDialog.isCaseSensitive(),  # cs
-                self.findDialog.isWholeWord(),   # wo
-                True,   # wrap
-                True,   # forward
-                )
+                       False,  # re
+                       self.findDialog.isCaseSensitive(),  # cs
+                       self.findDialog.isWholeWord(),   # wo
+                       True,   # wrap
+                       True,   # forward
+                       )
         return
 
     def findNext(self):
         line, index = self.getCursorPosition()
         self.findFirst(self.find_text,
-                False,  # re
-                self.findDialog.isCaseSensitive(),  # cs
-                self.findDialog.isWholeWord(),   # wo
-                True,   # wrap
-                True,   # forward
-                line, index
-                )
+                       False,  # re
+                       self.findDialog.isCaseSensitive(),  # cs
+                       self.findDialog.isWholeWord(),   # wo
+                       True,   # wrap
+                       True,   # forward
+                       line, index
+                       )
         return
 
     def findPrevious(self):
         line, index = self.getCursorPosition()
         index -= len(self.find_text)
         self.findFirst(self.find_text,
-                False,  # re
-                self.findDialog.isCaseSensitive(),  # cs
-                self.findDialog.isWholeWord(),   # wo
-                True,   # wrap
-                False,   # forward
-                line, index
-                )
+                       False,  # re
+                       self.findDialog.isCaseSensitive(),  # cs
+                       self.findDialog.isWholeWord(),   # wo
+                       True,   # wrap
+                       False,   # forward
+                       line, index
+                       )
         return
 
     def setStyle(self, filename):
@@ -204,6 +248,8 @@ class Editor(QsciScintilla):
                 lexer = QsciLexerPython(self)
             elif ext in ['.sh']:
                 lexer = QsciLexerBash(self)
+            elif ext in ['.rst', '.rest']:
+                lexer = MyLexerRest(self)
         if lexer:
             lexer.setDefaultFont(QtGui.QFont('Monospace', 12))
             self.setLexer(lexer)
