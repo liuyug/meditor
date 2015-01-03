dd52164fba30491698d8a7c43e5f49f7defcc65f
liuyug <liuyug@gmail.com>
2015-01-03 22:13:15 +0800

    Fix app data_path on win32

--- rsteditor/__init__.py
+++ rsteditor/__init__.py
@@ -5,8 +5,17 @@ __app_name__ = 'RSTEditor'
 __app_version__ = '0.1.6.4'
 __default_filename__ = 'unknown.rst'
 
-
 __data_path__ = os.path.join(sys.prefix, 'share', __app_name__.lower())
-if not os.path.exists(__data_path__):
-    __data_path__ = os.path.join(os.path.expanduser('~'), '.local', 'share', __app_name__.lower())
-__home_data_path__ = os.path.join(os.path.expanduser('~'), '.config', __app_name__.lower())
+if sys.platform == 'win32':
+    __data_path__ = os.path.join(os.path.dirname(__file__),
+                                 'share',
+                                 __app_name__.lower())
+else:
+    if not os.path.exists(__data_path__):
+        __data_path__ = os.path.join(os.path.expanduser('~'),
+                                     '.local',
+                                     'share',
+                                     __app_name__.lower())
+__home_data_path__ = os.path.join(os.path.expanduser('~'),
+                                  '.config',
+                                  __app_name__.lower())
