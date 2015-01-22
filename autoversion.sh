1ee5389e452eae7716dc27ccd1947d6209b4e192
liuyug <liuyug@gmail.com>
2015-01-22 10:43:47 +0800

    Add autoversion.sh

---
+++ autoversion.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+rsteditor_version_file='rsteditor/__init__.py'
+nsis_file='inst_script.nsi'
+
+usage()
+{
+    echo "autoversion.sh [new_version]"
+    echo ""
+    echo "Current version:" $version
+}
+
+check_version()
+{
+    version=`cat $rsteditor_version_file | grep 'app_version' | cut -d\' -f 2`
+}
+
+update_rsteditor_version()
+{
+    sed  -i -r "s/^(__app_version__).*$/\1 = '$new_version'/" $rsteditor_version_file
+}
+
+update_nsis_version()
+{
+    sed  -i -r "s/^(!define PRODUCT_VER).*$/\1 \"$new_version\"/" $nsis_file
+}
+
+update_git_tag()
+{
+    git tag $new_version
+}
+
+update_version()
+{
+    update_rsteditor_version
+    update_nsis_version
+    update_git_tag
+}
+
+check_version
+
+if [ "x$1" = "x" ]; then
+    usage
+    exit 0
+fi
+
+new_version=$1
+
+echo "Old version:" $version
+echo "New version:" $new_version
+
+update_version
+
+# vim: tabstop=4 shiftwidth=4 expandtab
