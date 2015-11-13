9538d9b19dab1ef66ae7b1c43b145d8d44c9f792
liuyug <liuyug@gmail.com>
2015-11-13 10:53:05 +0800

    import function

--- setup_cxFreeze.py
+++ setup_cxFreeze.py
@@ -7,7 +7,6 @@
 
 import os
 import sys
-import fnmatch
 
 from cx_Freeze import setup, Executable
 from distutils.core import Extension
@@ -20,6 +19,7 @@ import PyQt5
 
 from rsteditor import __app_name__
 from rsteditor import __app_version__
+from rsteditor.util import get_include_files
 
 
 build_ext_base = sipdistutils.build_ext
@@ -34,36 +34,6 @@ else:
     qt_path = '/usr/include'
 pyqt_path = os.path.dirname(PyQt5.__file__)
 docutils_path = os.path.dirname(docutils.__file__)
-
-
-def myglob(root_path, patterns, rel_path=None):
-    """
-    pattern: ['*.c', '*.cpp', '*.css', ...]
-    """
-    matches = []
-    for root, dirnames, filenames in os.walk(root_path):
-        for pattern in patterns:
-            for filename in fnmatch.filter(filenames, pattern):
-                abs_path = os.path.join(root, filename)
-                if rel_path:
-                    matches.append(os.path.relpath(abs_path, rel_path))
-                else:
-                    matches.append(abs_path)
-    return matches
-
-
-def get_include_files(src, patterns, dest):
-    """
-    return: [(src_file, dest_file), ...]
-    """
-    files = []
-    for filename in myglob(src, patterns, src):
-        files.append([
-            os.path.join(src, filename),
-            os.path.join(dest, filename)
-        ])
-    return files
-
 
 include_files = []
 include_files += get_include_files(
