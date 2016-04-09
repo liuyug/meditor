f1337a3b40f086cc72d6a19ee33b4b328ac74983
U-IBM194-P16TCDD\liuyug <liuyug@gmail.com>
2016-04-10 06:59:28 +0800

    use pyinstaller pack

--- pack_win32.cmd
+++ pack_win32.cmd
@@ -1,22 +1,14 @@
-rem create win32 executable with cx_Freeze and pack installer with NSIS
+cls
 
-set BUILD_DIR="build"
-set TARGET_DIR="%BUILD_DIR%\exe.win32-3.4"
+rmdir build /s /q
 
-set PYTHON="c:\python34\python"
-set ZIP7="c:\Program Files\7-Zip\7z.exe"
-set MAKENSIS="c:\Program Files (x86)\NSIS\Bin\makensis.exe"
+rem python setup.py bdist
 
-rmdir /s /q %TARGET_DIR%
+rem python setup.py build_ext --inplace
 
-%PYTHON% setup_cxFreeze.py build
-%ZIP7% x -y -o%TARGET_DIR% %TARGET_DIR%\library.zip docutils rsteditor
-%ZIP7% d %TARGET_DIR%\library.zip docutils rsteditor
+rem pyinstaller --icon rsteditor\share\pixmaps\rsteditor-text-editor.ico --clean --noupx --noconfirm --windowed rsteditor.py
 
-rem delete unused debug files
-rem del %TARGET_DIR%\imageformats\*d.pdb
-rem del %TARGET_DIR%\imageformats\*d.dll
-rem del %TARGET_DIR%\platforms\*d.pdb
-rem del %TARGET_DIR%\platforms\*d.dll
+pyinstaller --clean --noconfirm rsteditor.spec
 
-%MAKENSIS% inst_script.nsi
+rem set MAKENSIS="c:\Program Files (x86)\NSIS\Bin\makensis.exe"
+rem %MAKENSIS% inst_script.nsi
---
+++ rsteditor.spec
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['rsteditor.py'],
+             pathex=['D:\\Projects\\dev\\rsteditor-qt'],
+             binaries=None,
+             datas=[('rsteditor\share', 'share')],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='rsteditor',
+          debug=False,
+          strip=False,
+          upx=False,
+          console=False , icon='rsteditor\\share\\pixmaps\\rsteditor-text-editor.ico')
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=False,
+               name='rsteditor')
