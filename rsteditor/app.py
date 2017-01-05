fa11e5b8181458403c99537feb3e936a940ca760
LIU Yugang <liuyug@gmail.com>
2017-01-05 13:24:09 +0800

    fix

--- rsteditor/app.py
+++ rsteditor/app.py
@@ -934,8 +934,9 @@ def main():
     QtWidgets.QApplication.addLibraryPath(os.path.join(qt_path, 'PyQt5', 'Qt', 'plugins'))
     rstfile = toUtf8(os.path.realpath(args.rstfile)) if args.rstfile else None
     QtWidgets.QApplication.setStyle(args.style)
+    if sys.platform == 'win32':
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
-    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     logger.debug('qt plugin path: ' + ', '.join(app.libraryPaths()))
     win = MainWindow()
     win.loadFile(rstfile)
