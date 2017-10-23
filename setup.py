4c4ceec38860c1aad60e9e3f8242156a2e0685f7
LIU Yugang <liuyug@gmail.com>
2017-10-23 15:53:41 +0800

    replace fixed desktop file

--- setup.py
+++ setup.py
@@ -2,9 +2,32 @@
 # -*- encoding:utf-8 -*-
 
 from setuptools import setup
+from setuptools.command.install import install
 
 from meditor import __app_name__
 from meditor import __app_version__
+
+
+desktop = """[Desktop Entry]
+Version=%s
+Name=Markup Editor
+Comment=Editor for reStructedText and Markdown
+Exec=meditor
+MimeType=text/plain;
+Icon=meditor-text-editor
+Terminal=false
+Type=Application
+Categories=Application;Office;
+StartupNotify=true
+""" % __app_version__
+
+
+class install_desktop(install):
+    def run(self):
+        desktop_path = 'meditor.desktop'
+        with open(desktop_path, 'wt') as f:
+            f.write(desktop)
+        install.run(self)
 
 
 with open('README.rst') as f:
@@ -25,10 +48,17 @@ setup(
         'meditor.scilib',
     ],
     include_package_data=True,
+    data_files=[
+        ('share/pixmaps', ['meditor/share/pixmaps/meditor-text-editor.ico']),
+        ('share/applications', ['meditor.desktop'])
+    ],
     entry_points={
         'console_scripts': [
             'meditor = meditor.app:main',
         ],
+    },
+    cmdclass={
+        'install': install_desktop,
     },
     zip_safe=False,
 )
