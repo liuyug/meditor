3c3c879aed929cec9f86501d9f011d26e5a079d9
LIU Yugang <liuyug@gmail.com>
2017-12-21 12:45:13 +0800

    fix

--- meditor/scilib/__init__.py
+++ meditor/scilib/__init__.py
@@ -1,9 +1,3 @@
 from .input import _SciImSupport
 from .scilexerdefault import QsciLexerDefault
-
-try:
-    from .scilexerrest import QsciLexerRest
-    print('[INFO] Use C++ rst lexer')
-except Exception:
-    from .scilexerrest_py import QsciLexerRest
-    print('[INFO] Use PYTHON rst lexer')
+from .scilexerrest_py import QsciLexerRest
