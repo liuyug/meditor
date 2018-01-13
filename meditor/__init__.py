cc60c513e99934852333c249421f251c581c1c0b
LIU Yugang <liuyug@gmail.com>
2018-01-13 11:57:55 +0800

    version

--- meditor/__init__.py
+++ meditor/__init__.py
@@ -3,7 +3,7 @@ import os.path
 from collections import OrderedDict
 
 __app_name__ = 'Markup Editor'
-__app_version__ = '2.12.18'
+__app_version__ = '2.12.19'
 __default_basename__ = 'unknown'
 __app_path__ = 'meditor'
 
