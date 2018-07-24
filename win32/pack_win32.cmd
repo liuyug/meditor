0a78f88777605322ce99d9117c801402d3c23da4
LIU Yugang <liuyug@gmail.com>
2018-07-24 21:09:13 +0800

    fix missing module

--- win32/pack_win32.cmd
+++ win32/pack_win32.cmd
@@ -5,6 +5,6 @@ pushd ..
 rmdir build /s /q
 rmdir dist\meditor /s /q
 
-pyinstaller --clean --noconfirm --noconsole --name meditor --icon "meditor\data\meditor-text-editor.ico" --add-data "meditor\data;data" run.py
+pyinstaller --clean --noconfirm --noconsole --name meditor --icon "meditor\data\meditor-text-editor.ico" --add-data "meditor\data;data" --hidden-import PyQt5.sip run.py
 
 popd
