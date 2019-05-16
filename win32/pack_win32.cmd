e3cdfd1dc054da30a951297b2adf06cfd6876ecb
LIU Yugang <liuyug@gmail.com>
2019-05-16 22:41:36 +0800

    fix for pyqt5 5.12.3

--- win32/pack_win32.cmd
+++ win32/pack_win32.cmd
@@ -5,6 +5,21 @@ pushd ..
 rmdir build /s /q
 rmdir dist\meditor /s /q
 
-pyinstaller --clean --noconfirm --noconsole --name meditor --icon "meditor\data\meditor-text-editor.ico" --add-data "meditor\data;data" --hidden-import PyQt5.sip run.py
+pyinstaller --clean --noconfirm --noconsole^
+ --name meditor^
+ --icon "meditor\data\meditor-text-editor.ico"^
+ --add-data "meditor\data;data"^
+ --exclude-module ipython^
+ --exclude-module matplotlib^
+ --exclude-module numpy^
+ --exclude-module pandas^
+ --exclude-module jedi^
+ --exclude-module jinja2^
+ --exclude-module lxml.etree^
+ --exclude-module sqlalchemy^
+ --exclude-module sqlite3^
+ --exclude-module pydoc^
+ --exclude-module pycparser^
+ run.py
 
 popd
